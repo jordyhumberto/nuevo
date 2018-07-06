3686e8389bd27b1bcab7a96d7545dfef3b591b7f
jordy <jordyhvillavicencio@gmail.com>
2018-07-06 14:00:23 -0500

    algo mas

--- nav.php
+++ nav.php
@@ -1,23 +1,16 @@
 <header id="header">
-	
-	<div class="banner">
-        <a href="../../welcome.php#slider">
-            <img class="l_upein" src="../../img/logo_upein.png" alt="Logo_upein">
-        </a>
-		<div class="p_p">
-			<p class="p_upein">
-                <a href="../../welcome.php" style="text-decoration:none;color:white;">
-                    UNIVERSIDAD PERUANA DE</br>INVESTIGACIÓN Y NEGOCIOS
-                </a>
-            </p>	
-		</div>
-	</div>
+    <div class="banner">
+        <img class="l_upein" src="../../img/logo_upein.png" alt="Logo_upein">
+        <div class="p_p">
+            <p class="p_upein">INTRANET</p><p class="p_otro">UPEIN</p>	
+        </div>
+    </div>
 </header>
 <nav>
-	<ul>
-		<li><a href="#">Mantenimientos</a>
-			<ul>
-				<li><a href="#">General</a>
+    <ul>
+        <li><a>Mantenimientos&nbsp<i class="fas fa-edit"></i></span></a>
+            <ul>
+                <li><a>General</a>
                     <ul>
                         <li><a href="../../mantenimiento/general/m_g_alumno.php">Alumno</a></li>
                         <li><a href="../../mantenimiento/general/m_g_colegio.php">Colegio</a></li>
@@ -26,7 +19,7 @@
                         <li><a href="../../mantenimiento/general/m_g_tipo.php">Tipo de Pago</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Academico</a>
+                <li><a>Academico</a>
                     <ul>
                         <li><a href="../../mantenimiento/academico/m_a_ciclo.php">Ciclos</a></li>
                         <li><a href="../../mantenimiento/academico/m_a_curso.php">Cursos</a></li>
@@ -36,7 +29,7 @@
                         <li><a href="../../mantenimiento/academico/m_a_curso_docente.php">Cursos-Docentes</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Administrativo</a>
+                <li><a>Administrativo</a>
                     <ul>
                         <li><a href="../../mantenimiento/administrativo/m_ad_beca.php">Becas</a></li>
                         <li><a href="../../mantenimiento/administrativo/m_ad_facultad.php">Facultad</a></li>
@@ -50,30 +43,30 @@
                         <li><a href="#">Documento D' Impresión</a></li>
                     </ul>
                 </li>
-			</ul>
-		</li>
-		<li><a href="#">Procesos</a>
-			<ul>
-                <li><a href="#">Matricula</a>
+            </ul>
+        </li>
+        <li><a>Procesos&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fas fa-edit"></i></a>
+            <ul>
+                <li><a>Matricula</a>
                     <ul>
                         <li><a href="../../proceso/matricula/p_m_matricula.php">Matricula</a></li>
                         <li><a href="#">Anular-Matricula</a></li>
                     </ul>
                 </li>
-                <li ><a href="#">Descuentos</a>
+                <li ><a>Descuentos</a>
                     <ul>
                         <li><a href="#">Descuentos</a></li>
                         <li><a href="#">Anular-Descuentos</a></li>
                     </ul>
                 </li>
-                <li ><a href="#">Pagos</a>
+                <li ><a>Pagos</a>
                     <ul>
                         <li><a href="#">Pagos</a></li>
                         <li><a href="#">Adelantos</a></li>
                         <li><a href="#">Anular-Pagos</a></li>
                     </ul>
                 </li>
-                <li ><a href="#">Academico</a>
+                <li ><a>Academico</a>
                     <ul>
                         <li><a href="../../proceso/academico/p_a_cursoo.php">Curso-Operativo</a></li>
                         <li><a href="#">Horario-Clases</a></li>
@@ -82,38 +75,39 @@
                         <li><a href="#">Registro-Notas-Admin</a></li>
                     </ul>
                 </li>
-				<li ><a href="#">Consultas Notas</a></li>
-                    <li ><a href="#">Impresión</a>
-                        <ul>
-                            <li><a href="">Conf. de Impresión</a></li>
-                            <li><a href="">Impresión</a></li>
-                        </ul>
-                    </li>
-				</ul>
-			</li>
-			<li><a href="#">Consultas</a>
-				<ul>
-                    <li><a href="../../consultas/alumnos/c_a_alumnos.php">Alumnos</a></li>
-					<li><a href="../../consultas/pagoalumnos/c_pgs_pagoalumnos.php">CompromisoPagos Alumnos</a></li>
-					<li><a href="../../consultas/pagofecha/c_pf_pagofecha.php">Pagos por Fecha</a></li>
-					<li><a href="../../consultas/matricula/c_m_matricula.php">Matricula Alumno</a></li>
-                    <li><a href="../../consultas/pagoalumno/c_pg_pagoalumno.php">Pago Alumnos</a></li>
-					<li><a href="../../consultas/verpago/c_v_verpago.php">Ver Pagos</a></li>
-					<li><a href="../../consultas/notasalumno/c_n_notasalumno.php">Notas por Alumno</a></li>
-					<li><a href="#">Pagos por Tipo</a></li>
-				</ul>					
-			</li>
-			<li><a href="#">Reportes</a>
-				<ul>
-					<li><a href="#">Pagos por Proceso</a></li>
-					<li><a href="#">Historico de Pagos</a></li>
-					<li><a href="#">Notas por cuso ope.</a></li>
-					<li><a href="#">Notas Registradas.</a></li>
-					<li><a href="#">Horario Alumnos.</a></li>
-					<li><a href="#">Horario Docente.</a></li>
-					<li><a href="#">Horario Aula.</a></li>
-				</ul>
-			</li>
-		<li><a href="../../logout.php">Salir</a></li>
-	</ul>	
+                <li ><a href="#">Consultas Notas</a></li>
+                <li ><a href="#">Impresión</a>
+                    <ul>
+                        <li><a href="">Conf. de Impresión</a></li>
+                        <li><a href="">Impresión</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+            <!--concentrarme en estos dos-->
+        <li><a>Consultas&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fas fa-edit"></i></a>
+            <ul>
+                <li><a href="../../consultas/alumnos/c_a_alumnos.php">Alumnos</a></li>
+                <li><a href="../../consultas/pagoalumnos/c_pgs_pagoalumnos.php">Compromiso Alumnos</a></li>
+                <li><a href="../../consultas/pagofecha/c_pf_pagofecha.php">Pagos por Fecha</a></li>
+                <li><a href="../../consultas/matricula/c_m_matricula.php">Matricula Alumno</a></li>
+                <li><a href="../../consultas/pagoalumno/c_pg_pagoalumno.php">Pago Alumnos</a></li>
+                <li><a href="../../consultas/verpago/c_v_verpago.php">Ver Pagos</a></li>
+                <li><a href="../../consultas/notasalumno/c_n_notasalumno.php">Notas por Alumno</a></li>
+                <li><a href="#">Pagos por Tipo</a></li>
+            </ul>					
+        </li>
+        <li><a>Reportes&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fas fa-edit"></i></a>
+            <ul>
+                <li><a href="#">Pagos por Proceso</a></li>
+                <li><a href="#">Historico de Pagos</a></li>
+                <li><a href="#">Notas por cuso ope.</a></li>
+                <li><a href="#">Notas Registradas.</a></li>
+                <li><a href="#">Horario Alumnos.</a></li><!--no sirve-->
+                <li><a href="#">Horario Docente.</a></li><!--no sirve-->
+                <li><a href="#">Horario Aula.</a></li><!--no sirve-->
+            </ul>
+        </li>
+        <li><a href="../../logout.php">Salir</a></li>
+    </ul>	
 </nav>
